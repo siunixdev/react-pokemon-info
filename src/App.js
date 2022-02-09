1914e2bc5fbd79887f25eadc94f1f8f6b1aa5c38
siunix <abd.siunix@gmail.com>
2022-02-09 21:57:31 +0700

    Add and implement react-router-dom in project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      <h3 className='text-2xl'>Pokemon info</h3>
-    </div>
+    <Router>
+      <div className="App">
+        <h3 className='text-2xl'>Pokemon info</h3>
+      </div>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
