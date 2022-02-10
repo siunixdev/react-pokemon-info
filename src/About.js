26976c67b5cabe8e78b46ebdf10dd1733835e6fa
siunix <abd.siunix@gmail.com>
2022-02-10 21:35:27 +0700

    Set slug to call Pokemon detail

--- src/About.js
+++ src/About.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import LazyLoad from 'react-lazyload'
 
 function About() {
   const { slug } = useParams()
+  const [pokemon, setPokemon] = useState()
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${slug}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data)
+        console.log(data);
+      })
+  }, [slug])
 
   return (
     <div className='flex flex-wrap'>
-      <div>Pokemon Info {slug}</div>
+      {
+        pokemon && (
+          <div className='bg-white p-8 shadow-lg rounded-lg'>
+            <h3 className='text-center'>{pokemon.name}</h3>
+            <div className='flex'>
+              <LazyLoad height={200}>
+                <img src={pokemon.sprites['front_shiny']} height={200} />
+              </LazyLoad>
+              <LazyLoad height={200}>
+                <img src={pokemon.sprites['back_shiny']} height={200} />
+              </LazyLoad>
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 
 function Home({ pokemon: results }) {
   return (
-    <div className='flex flex-wrap'>
+    <div className='grid grid-cols-6 gap-2'>
       {
         results && results.map((val, id) => (
-          <div className='text-blue-400 mr-2' key={id}>
-            <Link to={`/about/${val.id}`}>{val.name}</Link>
+          <div className='p-4 bg-white rounded-lg'>
+            <div className='text-blue-400 mr-2' key={id}>
+              <Link to={`/about/${val.id}`}>{val.name}</Link>
+            </div>
           </div>
         ))
       }
